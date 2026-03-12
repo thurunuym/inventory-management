f80d82997dec1785693ca528f8203bdd8f76cd1e
Thurunu Marasinghe <106548569+thurunuym@users.noreply.github.com>
2026-03-13 02:30:07 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/authContext'
+import { AuthProvider } from './context/Context'
 import App from './App'
 import './App.css'
 
