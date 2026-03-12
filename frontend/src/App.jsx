50fd7055b89f292584ff60293cf6373ad3c363ca
thurunuym <marasinghe.ty@gmail.com>
2026-03-12 23:27:08 +0530

    error fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Header from './components/Header'
 import BorrowItem from './pages/Borrow'
 import ReturnItem from './pages/Return'
 import Users from './pages/Users'
-import { useAuth } from './context/AuthContext'
+import { useAuth } from './context/AuthContext.jsx'
 
 function App() {
   const { user, loading } = useAuth()
