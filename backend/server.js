de9c70d75fbc12b647ef507490c9e98e34549384
thurunuym <marasinghe.ty@gmail.com>
2026-03-12 23:01:22 +0530

    deploying settings added

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,12 @@ app.use('/api/inventory', itemRoutes);
 app.use('/api/inventory', storageRoutes);
 app.use('/api', borrowRoutes);
 
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
